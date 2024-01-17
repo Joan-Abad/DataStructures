acc9add259eb38fe4862ab1e97e1b294ae1db690
Joan Abad <j.abad.reynoso@gmail.com>
2024-01-17 15:55:13 +0100

    Search Node Linear function added, with also a version of SearchNodeMTH, which moves the found object to the head of the linked list. Optimization technique for future lookups.

--- Source/Public/LinkedList.h
+++ Source/Public/LinkedList.h
@@ -62,6 +62,7 @@ public:
 		return size; 
 	}
 
+	//Summs all the elements and return the total sum. The operator + must be available for the Type of the nodes of the Linked List. 
 	Type SumOfAllElements()
 	{
 		Type returnValue = 0;
@@ -76,20 +77,79 @@ public:
 		return returnValue;
 	}
 
+	//Gets the maximum value of all the nodes on the linked list. Operator > should be declared for the Type of the nodes of the Linked List. 
 	Type GetMaximumValue()
 	{
 		Node<Type>* node = headNode;
 		Type returnValue = node->value;
 
-
 		while (node)
 		{
-			if (returnValue < node->value)
+			if (node->value > returnValue)
 				returnValue = node->value;
 
 			node = node->GetNextNode();
 		}
 
 		return returnValue;
+	}
+
+	//Checks if a specific value is on the Linked List. 
+	bool HasValue(Type ValueToSearch)
+	{
+		Node<Type>* node = headNode;
+
+		while (node)
+		{
+			if (node->value == ValueToSearch)
+				return true;
+
+			node = node->GetNextNode();
+		}
+
+		return false; 
+	}
+	//Searches for the first node that has the parameter value and returns it if any. 
+	Node<Type>* SearchNode(Type ValueToSearch)
+	{
+		Node<Type>* intNode = headNode;
+
+		while (intNode)
+		{
+			if (intNode->value == ValueToSearch)
+				return intNode;
+		
+			intNode = intNode->GetNextNode();
+		}
+
+		return nullptr; 
+	}
+
+	//Searches for the note associated with that value, and it puts it at the top of the linked list for quicker future access
+	//This uses a MoveToHead metodology
+	Node<Type>* SearchNodeMTH(Type ValueToSearch)
+	{
+		int i = 1; 
+		Node<Type>* intNode = headNode;
+		Node<Type>* q = nullptr;
+
+		while (intNode)
+		{
+			if (intNode->value == ValueToSearch)
+			{
+				if (q)
+				{
+					q->NextNode = intNode->GetNextNode();
+					intNode->NextNode = headNode;
+					headNode = intNode;
+				}
+				return headNode;
+			}
+			q = intNode; 
+			intNode = intNode->GetNextNode();
+			i++;
+		}
+
+		return nullptr;
 	}
 };
