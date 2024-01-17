7e18193c100a990d71a2aaf38318d20531fe5be7
Joan Abad <j.abad.reynoso@gmail.com>
2024-01-17 14:06:09 +0100

    Linked List size function done.

--- Source/Public/LinkedList.h
+++ Source/Public/LinkedList.h
@@ -25,14 +25,16 @@ private:
 public: 
 	LinkedList() : headNode(nullptr) {};
 	
-
+	//Add a node at the front of the list
 	void push_front(Type type)
 	{
+		//Create new node
 		Node<Type>* newNode = new Node<Type>(type);
 		newNode->NextNode = headNode;
 		headNode = newNode;
 	}
 
+	//Display the content of the nodes
 	void Display()
 	{
 		Node<Type>* node = headNode;
@@ -44,5 +46,19 @@ public:
 			node = node->GetNextNode();
 			i++;
 		}
+	}
+
+	//Gets the amount of nodes attached to this Linked List
+	size_t Size()
+	{
+		size_t size = 0;
+		Node<Type>* node = headNode;
+
+		while (node != nullptr)
+		{
+			size++;
+			node = node->GetNextNode();
+		}
+		return size; 
 	}
 };
